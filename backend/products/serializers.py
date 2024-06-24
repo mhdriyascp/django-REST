0c231838f7affcfa2cda2850c1ba6ef94dacd624
MR <mr@MacBook-Pro.lan>
2024-06-24 14:12:41 +0530

    api commit

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from .models import Product
 
 
@@ -8,6 +9,8 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [
+            'url',
+            'pk',
             'title',
             'content',
             'price',
@@ -17,4 +20,18 @@ class ProductSerializer(serializers.ModelSerializer):
 
     # def get_my_discounts(self, obj):
     #     return obj.get_discounts()
+
+    def get_url(self, obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse("product-detail", kwargs={"pk": obj.pk}, request=request)
+
+    def validate_title(self, value):
+        qs = Product.objects.filter(title__iexact=value)
+        if qs.exists():
+            raise serializers.ValidationError(f"Title {value} already exists")
+        return value
+    
+
 
--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_create_view),
-    path('<int:pk>/update', views.product_update_view),
-    path('<int:pk>/delete', views.product_destroy_view),
-    path('<int:pk>/', views.product_detail_view),
-    path('list/', views.product_list_view),
+    path('', views.product_create_view, name='product-create'),
+    path('<int:pk>/update', views.product_update_view, name='product-update'),
+    path('<int:pk>/delete', views.product_destroy_view, name='product-destroy'),
+    path('<int:pk>/', views.product_detail_view, name='product-detail'),
+    path('list/', views.product_list_view, name='product-list'),
 ]
